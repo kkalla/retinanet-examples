87ba862f2ebac46383b2b9648b7a1e497c603175
kkalla <dkswns333@gmail.com>
2020-06-15 09:01:50 +0000

    Add docker files

--- run_odtk_container.sh
+++ run_odtk_container.sh
@@ -1,6 +1,17 @@
+# train
+# sudo docker run --runtime=nvidia \
+# -e NVIDIA_VISIBLE_DEVICES=1,2 \
+# --ipc=host \
+# -v /nas/workspace/1_user/dkswns333@agilesoda.ai/object_detection/retinanet-examples/workspace:/my-workspace \
+# -v /nas/workspace/1_user/dkswns333@agilesoda.ai/datasets/COCO2017:/coco2017 \
+# -d \
+# odtk:train
+
+# eval
 sudo docker run --runtime=nvidia \
--e NVIDIA_VISIBLE_DEVICES=3,4,5,7 \
---rm --ipc=host -d \
--v /nas/workspace/1_user/dkswns333@agilesoda.ai/object_detection/workspace:/my-workspace \
+-e NVIDIA_VISIBLE_DEVICES=1,2 \
+--ipc=host \
+-v /nas/workspace/1_user/dkswns333@agilesoda.ai/object_detection/retinanet-examples/workspace:/my-workspace \
 -v /nas/workspace/1_user/dkswns333@agilesoda.ai/datasets/COCO2017:/coco2017 \
-odtk:latest
+-i \
+odtk:eval
